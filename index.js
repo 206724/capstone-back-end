6a54a2d6363c115835f059f841be760ec9ca3524
Girma <93812274+206724@users.noreply.github.com>
2024-07-27 08:32:09 -0700

    commit

--- index.js
+++ index.js
@@ -152,25 +152,47 @@ app.post('/upload',photoMiddleware.array('photos',100),(req,res) =>{
 
 
 
-app.post('/places' , (req,res) =>  {
-  const{token} =req.cookies;
+app.put('/places', async (req,res) => {
 
-  const{
-      title,address,addPhotos,description,
-    perks,extraInfo,checkIn,checkOut,maxGuests,} = req.body;
+  const {token} = req.cookies;
+  const {
+    id, title,address,addedPhotos,description,
+    perks,extraInfo,checkIn,checkOut,maxGuests,price,
+  } = req.body;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     if (err) throw err;
-    const placeDoc =await Place.create({
+    const placeDoc = await Place.findById(id);
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+        title,address,photos:addedPhotos,description,
+        perks,extraInfo,checkIn,checkOut,maxGuests,price,
+      });
+      await placeDoc.save();
+      res.json('ok');
+    }
+  });
+});
 
-      owner:userData.id,
-      title,address,photos:addPhotos,description,
+app.post('/user-places', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  const {
+    title,address,addedPhotos,description,price,
+    perks,extraInfo,checkIn,checkOut,maxGuests,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner:userData.id,price,
+      title,address,photos:addedPhotos,description,
       perks,extraInfo,checkIn,checkOut,maxGuests,
-    })
-   res.json(placeDoc);
-     
-    })
+    });
+    res.json(placeDoc);
+  });
+});
 
-})
+
+
 
 
 app.get('/user-places',(req,res) =>{
@@ -190,9 +212,9 @@ res.json(await Place.findById(id));
 
 
  
-app.put('/places', async (req,res) => {
+app.get('/places', async (req,res) => {
  
-  res.json(await Place.find)
+  res.json(await Place.find())
 });
 
 // Start the server
