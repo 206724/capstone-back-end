2d12b499071a3862233b2c4cf6f87aee9907576e
Girma <93812274+206724@users.noreply.github.com>
2024-07-29 01:25:25 -0700

    commit

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ app.post('/bookings', async (req, res) => {
 });
 
 
-
+//all the booking *my booking
 app.get('/bookings', async (req,res) => {
 
   const userData = await getUserDataFromReq(req);
@@ -260,9 +260,17 @@ app.get('/bookings', async (req,res) => {
 });
 
 
+
+
+
+
+
+
+
+
+
 // Start the server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
